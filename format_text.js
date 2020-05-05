0ecbce5fbd68af341b3c11f904c4baf403c7b52f
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-05-06 00:10:43 +0300

    Update format_text.js

--- format_text.js
+++ format_text.js
@@ -4,14 +4,12 @@ var lnk;
 var BOARD;
 const lnk2=new RegExp(`href="//`, 'g')
 
-function format_text(text) {
-  return new Promise(function(resolve) {
+async function format_text(text) {
     text=text.replace(lnk, `"https://boards.4channel.org/${BOARD}/thread/`)
     text=text.replace(lnk2, `href="https://`)
     text=text.replace(br, '\n')
     text=text.replace(remove_junk, '')
-    resolve(text);
-  });
+    return text
 }
 function init(Board){
   BOARD=Board;
